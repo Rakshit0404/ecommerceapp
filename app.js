ed9b7503dbfcb1f50b79f2e602fcf154a4eb3b75
Rakshit0404 <saritasakshi52@gmail.com>
2022-02-07 21:13:02 +0530

    hiii

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
+const ejsmate = require('ejs-mate');
+const methodOverride = require('method-override');
 
 app.engine('ejs', ejsmate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'views/layouts')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',(req,res)=>{
-    res.send("Hello");
+    res.render('layouts/home.ejs');
 })
 
 app.listen(3000, (req, res)=>{
